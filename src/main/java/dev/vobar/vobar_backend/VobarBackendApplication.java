596aaa35005fa07f033d1ec9e0fc5c51f66710ec
vobar <vobarkal@gmail.com>
2026-05-09 21:13:53 +0200

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/dev/vobar/vobar_backend/VobarBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.vobar.vobar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VobarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VobarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vobar/vobar_backend/VobarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vobar.vobar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VobarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
